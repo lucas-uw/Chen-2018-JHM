5e8ed9ea777cb53387032d04f6ad4a0c07061ae4
Xiaodong Chen <xiaodc.work@gmail.com>
2017-06-06 12:28:20 -0700

    Uncomment the temporary comments used during debugging

--- hyak_tools/ECMWF/calc_percentile.m
+++ hyak_tools/ECMWF/calc_percentile.m
@@ -1,58 +1,56 @@
 %% CAPE
-%clear all;
-%clc;
-%nx = 256;
-%ny = 512;
-%load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/matlab_files/full_cape_data.mat');
-%cape_percentile = ones(nx,ny,2,1001)*-9999;
-%for i=1:nx
-%    i
-%    for j=1:ny
-%        data = squeeze(full_cape_data(:,i,j));
-%        cape_percentile(i,j,1,:) = prctile(data,(0:0.1:100));
-%        cape_percentile(i,j,2,:) = (0:0.001:1);
-%    end
-%end
-%save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/cape/cape_percentile.mat', '-v7.3', 'cape_percentile');
-%
-%
-%% PW
-%clear all;
-%clc;
-%nx = 256;
-%ny = 512;
-%load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/matlab_files/full_pw_data.mat');
-%pw_percentile = ones(nx,ny,2,1001)*-9999;
-%for i=1:nx
-%    i
-%    for j=1:ny
-%        data = squeeze(full_pw_data(:,i,j));
-%        pw_percentile(i,j,1,:) = prctile(data,(0:0.1:100));
-%        pw_percentile(i,j,2,:) = (0:0.001:1);
-%    end
-%end
-%save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/pw/pw_percentile.mat', '-v7.3', 'pw_percentile');
-%
-%
-%% wind
-%clear all;
-%clc;
-%nx = 256;
-%ny = 512;
-%load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/matlab_files/full_wind_data.mat');
-%wind_percentile = ones(nx,ny,2,1001)*-9999;
-%for i=1:nx
-%    i
-%    for j=1:ny
-%        data = squeeze(full_wind_data(:,i,j));
-%        wind_percentile(i,j,1,:) = prctile(data,(0:0.1:100));
-%        wind_percentile(i,j,2,:) = (0:0.001:1);
-%    end
-%end
-%save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/wind/wind_percentile.mat', '-v7.3', 'wind_percentile');
+clear all;
+clc;
+nx = 256;
+ny = 512;
+load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/matlab_files/full_cape_data.mat');
+cape_percentile = ones(nx,ny,2,1001)*-9999;
+for i=1:nx
+    i
+    for j=1:ny
+        data = squeeze(full_cape_data(:,i,j));
+        cape_percentile(i,j,1,:) = prctile(data,(0:0.1:100));
+        cape_percentile(i,j,2,:) = (0:0.001:1);
+    end
+end
+save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/cape/cape_percentile.mat', '-v7.3', 'cape_percentile');
+
+% PW
+clear all;
+clc;
+nx = 256;
+ny = 512;
+load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/matlab_files/full_pw_data.mat');
+pw_percentile = ones(nx,ny,2,1001)*-9999;
+for i=1:nx
+    i
+    for j=1:ny
+        data = squeeze(full_pw_data(:,i,j));
+        pw_percentile(i,j,1,:) = prctile(data,(0:0.1:100));
+        pw_percentile(i,j,2,:) = (0:0.001:1);
+    end
+end
+save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/pw/pw_percentile.mat', '-v7.3', 'pw_percentile');
 
 
-%% RH
+% wind
+clear all;
+clc;
+nx = 256;
+ny = 512;
+load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/matlab_files/full_wind_data.mat');
+wind_percentile = ones(nx,ny,2,1001)*-9999;
+for i=1:nx
+    i
+    for j=1:ny
+        data = squeeze(full_wind_data(:,i,j));
+        wind_percentile(i,j,1,:) = prctile(data,(0:0.1:100));
+        wind_percentile(i,j,2,:) = (0:0.001:1);
+    end
+end
+save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/wind/wind_percentile.mat', '-v7.3', 'wind_percentile');
+
+% RH
 clear all;
 clc;
 nx = 256;
--- hyak_tools/ECMWF/calc_percentile_mono.m
+++ hyak_tools/ECMWF/calc_percentile_mono.m
@@ -1,86 +1,86 @@
-%%% CAPE
-%clear all;
-%clc;
-%load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/cape/cape_percentile.mat');
-%in_percentile_orig = cape_percentile;
-%clear cape_percentile;
-%out_percentile = ones(256,512,2,1001)*-9999;
-%out_percentile_count = ones(256,512)*-9999;
-%
-%for i=1:256
-%    i
-%    for j=1:512
-%        out_percentile(i,j,:,1) = in_percentile_orig(i,j,:,1);
-%        count = 2;
-%        for k=2:1001
-%            if in_percentile_orig(i,j,1,k)>in_percentile_orig(i,j,1,k-1)
-%                out_percentile(i,j,:,count) = in_percentile_orig(i,j,:,k);
-%                count = count + 1;
-%            end
-%        end
-%        out_percentile_count(i,j) = count-1;
-%    end
-%end
-%cape_percentile = out_percentile;
-%cape_percentile_count = out_percentile_count;
-%save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/cape/cape_percentile_mono.mat', '-v7.3', 'cape_percentile', 'cape_percentile_count');
-%
-%
-%%% PW
-%clear all;
-%clc;
-%
-%load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/pw/pw_percentile.mat');
-%
-%in_percentile_orig = pw_percentile;
-%clear pw_percentile;
-%
-%out_percentile = ones(256,512,2,1001)*-9999;
-%out_percentile_count = ones(256,512)*-9999;
-%
-%for i=1:256
-%    i
-%    for j=1:512
-%        out_percentile(i,j,:,1) = in_percentile_orig(i,j,:,1);
-%        count = 2;
-%        for k=2:1001
-%            if in_percentile_orig(i,j,1,k)>in_percentile_orig(i,j,1,k-1)
-%                out_percentile(i,j,:,count) = in_percentile_orig(i,j,:,k);
-%                count = count + 1;
-%            end
-%        end
-%        out_percentile_count(i,j) = count-1;
-%    end
-%end
-%pw_percentile = out_percentile;
-%pw_percentile_count = out_percentile_count;
-%save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/pw/pw_percentile_mono.mat', '-v7.3', 'pw_percentile', 'pw_percentile_count');
-%
-%%% wind
-%clear all;
-%clc;
-%load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/wind/wind_percentile.mat');
-%in_percentile_orig = wind_percentile;
-%clear wind_percentile;
-%out_percentile = ones(256,512,2,1001)*-9999;
-%out_percentile_count = ones(256,512)*-9999;
-%for i=1:256
-%    i
-%    for j=1:512
-%        out_percentile(i,j,:,1) = in_percentile_orig(i,j,:,1);
-%        count = 2;
-%        for k=2:1001
-%            if in_percentile_orig(i,j,1,k)>in_percentile_orig(i,j,1,k-1)
-%                out_percentile(i,j,:,count) = in_percentile_orig(i,j,:,k);
-%                count = count + 1;
-%            end
-%        end
-%        out_percentile_count(i,j) = count-1;
-%    end
-%end
-%wind_percentile = out_percentile;
-%wind_percentile_count = out_percentile_count;
-%save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/wind/wind_percentile_mono.mat', '-v7.3', 'wind_percentile', 'wind_percentile_count');
+%% CAPE
+clear all;
+clc;
+load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/cape/cape_percentile.mat');
+in_percentile_orig = cape_percentile;
+clear cape_percentile;
+out_percentile = ones(256,512,2,1001)*-9999;
+out_percentile_count = ones(256,512)*-9999;
+
+for i=1:256
+    i
+    for j=1:512
+        out_percentile(i,j,:,1) = in_percentile_orig(i,j,:,1);
+        count = 2;
+        for k=2:1001
+            if in_percentile_orig(i,j,1,k)>in_percentile_orig(i,j,1,k-1)
+                out_percentile(i,j,:,count) = in_percentile_orig(i,j,:,k);
+                count = count + 1;
+            end
+        end
+        out_percentile_count(i,j) = count-1;
+    end
+end
+cape_percentile = out_percentile;
+cape_percentile_count = out_percentile_count;
+save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/cape/cape_percentile_mono.mat', '-v7.3', 'cape_percentile', 'cape_percentile_count');
+
+
+%% PW
+clear all;
+clc;
+
+load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/pw/pw_percentile.mat');
+
+in_percentile_orig = pw_percentile;
+clear pw_percentile;
+
+out_percentile = ones(256,512,2,1001)*-9999;
+out_percentile_count = ones(256,512)*-9999;
+
+for i=1:256
+    i
+    for j=1:512
+        out_percentile(i,j,:,1) = in_percentile_orig(i,j,:,1);
+        count = 2;
+        for k=2:1001
+            if in_percentile_orig(i,j,1,k)>in_percentile_orig(i,j,1,k-1)
+                out_percentile(i,j,:,count) = in_percentile_orig(i,j,:,k);
+                count = count + 1;
+            end
+        end
+        out_percentile_count(i,j) = count-1;
+    end
+end
+pw_percentile = out_percentile;
+pw_percentile_count = out_percentile_count;
+save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/pw/pw_percentile_mono.mat', '-v7.3', 'pw_percentile', 'pw_percentile_count');
+
+%% wind
+clear all;
+clc;
+load('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/wind/wind_percentile.mat');
+in_percentile_orig = wind_percentile;
+clear wind_percentile;
+out_percentile = ones(256,512,2,1001)*-9999;
+out_percentile_count = ones(256,512)*-9999;
+for i=1:256
+    i
+    for j=1:512
+        out_percentile(i,j,:,1) = in_percentile_orig(i,j,:,1);
+        count = 2;
+        for k=2:1001
+            if in_percentile_orig(i,j,1,k)>in_percentile_orig(i,j,1,k-1)
+                out_percentile(i,j,:,count) = in_percentile_orig(i,j,:,k);
+                count = count + 1;
+            end
+        end
+        out_percentile_count(i,j) = count-1;
+    end
+end
+wind_percentile = out_percentile;
+wind_percentile_count = out_percentile_count;
+save('/civil/shared/ganges/xiaodc/lulcc/data/reanalysis_data/ECMWF/Interim/wind/wind_percentile_mono.mat', '-v7.3', 'wind_percentile', 'wind_percentile_count');
 
 %% RH
 clear all;
